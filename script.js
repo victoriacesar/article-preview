38e596a33701a70be679900ee35eb65fef1fff68
Victória César <victoriacesaras@gmail.com>
2021-05-22 14:37:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const shareBtn = document.querySelector('.info_button');
+const btnImg = document.querySelector('.btnImg');
+const shareBar = document.querySelector('.share');
+
+function changeBtnImg(button, imgSrc, thisClass){
+  if(button.classList.contains(thisClass)){
+    btnImg.src = imgSrc
+  } else if(!button.classList.contains(thisClass)){
+    btnImg.src = 'images/icon-share.svg'
+  }
+}
+
+const showShareBarMobile = () => {
+    shareBtn.classList.toggle('active')
+    shareBar.classList.toggle('active')
+    changeBtnImg(shareBtn, 'images/icon-share-active.svg', 'active')
+}
+
+const showShareBarDesktop = () => {
+  shareBtn.classList.toggle('active-desktop')
+  shareBar.classList.toggle('active-desktop')
+  changeBtnImg(shareBtn, 'images/icon-share-active.svg', 'active')
+}
+
+shareBtn.addEventListener('click', () => {
+  let windowWidth = window.innerWidth;
+  windowWidth < 1440 ? showShareBarMobile() : showShareBarDesktop();
+});
